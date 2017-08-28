03f0c6fd6d0f353e99a11a0b0d9ff6815da4d59a
Aditya Karra <aditya.ck@jaliansystems.com>
2017-08-28 17:33:54 +0530

    Fixed: Button bar alignment in AceEditorPreferencesStage.

--- marathon-core/src/main/java/net/sourceforge/marathon/fx/projectselection/ButtonBarX.java
+++ marathon-core/src/main/java/net/sourceforge/marathon/fx/projectselection/ButtonBarX.java
@@ -1,27 +1,32 @@
 package net.sourceforge.marathon.fx.projectselection;
 
 import javafx.collections.ObservableList;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.HBox;
 
-public class ButtonBarX extends TilePane {
+public class ButtonBarX extends HBox {
 
     public ButtonBarX() {
-        super(Orientation.HORIZONTAL, 5, 0);
-        setMinWidth(TilePane.USE_PREF_SIZE);
+        super(5);
         setAlignment(Pos.CENTER_RIGHT);
     }
 
     @Override protected void layoutChildren() {
-        ObservableList<Node> children = this.getChildren();
-        for (Node node : children)
-            if (node instanceof Button)
-                ((Button) node).setMaxWidth(Double.MAX_VALUE);
-        setPrefColumns(children.size());
+        double minPrefWidth = calculatePrefChildWidth();
+        setMinWidth(minPrefWidth * (getChildren().size() + 1));
+        for (Node n : getChildren())
+            if (n instanceof Button)
+                ((Button) n).setMinWidth(minPrefWidth);
         super.layoutChildren();
+    }
+
+    private double calculatePrefChildWidth() {
+        double minPrefWidth = 0;
+        for (Node n : getChildren())
+            minPrefWidth = Math.max(minPrefWidth, n.prefWidth(-1));
+        return minPrefWidth;
     }
 
     public ObservableList<Node> getButtons() {
