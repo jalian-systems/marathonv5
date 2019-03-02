3b4b480dcd84dd3c6d7b00396d9954e9b91e5720
Aditya Karra <aditya.ck@jaliansystems.com>
2019-03-02 17:00:49 +0530

    feat: added support to getByPseudoElement for ChoiceBoxElement.

--- marathon-javafx/marathon-javafx-agent/src/main/java/net/sourceforge/marathon/javafxagent/components/JavaFXChoiceBoxElement.java
+++ marathon-javafx/marathon-javafx-agent/src/main/java/net/sourceforge/marathon/javafxagent/components/JavaFXChoiceBoxElement.java
@@ -15,6 +15,9 @@
  ******************************************************************************/
 package net.sourceforge.marathon.javafxagent.components;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.json.JSONArray;
@@ -22,6 +25,7 @@ import org.json.JSONArray;
 import javafx.scene.Node;
 import javafx.scene.control.ChoiceBox;
 import net.sourceforge.marathon.javafxagent.IJavaFXAgent;
+import net.sourceforge.marathon.javafxagent.IJavaFXElement;
 import net.sourceforge.marathon.javafxagent.JavaFXElement;
 import net.sourceforge.marathon.javafxagent.JavaFXTargetLocator.JFXWindow;
 
@@ -31,6 +35,22 @@ public class JavaFXChoiceBoxElement extends JavaFXElement {
 
     public JavaFXChoiceBoxElement(Node component, IJavaFXAgent driver, JFXWindow window) {
         super(component, driver, window);
+    }
+
+    @Override
+    public List<IJavaFXElement> getByPseudoElement(String selector, Object[] params) {
+        if (selector.equals("nth-option")) {
+            return Arrays.asList(new JavaFXChoiceBoxOptionElement(this, ((Integer) params[0]).intValue() - 1));
+        } else if (selector.equals("all-options") || selector.equals("all-cells")) {
+            ChoiceBox<?> listView = (ChoiceBox<?>) getComponent();
+            ArrayList<IJavaFXElement> r = new ArrayList<>();
+            int nItems = listView.getItems().size();
+            for (int i = 0; i < nItems; i++) {
+                r.add(new JavaFXChoiceBoxOptionElement(this, i));
+            }
+            return r;
+        }
+        return super.getByPseudoElement(selector, params);
     }
 
     @Override
